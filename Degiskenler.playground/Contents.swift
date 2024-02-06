e3e6c8e432d5094402d1562e1567c8a7c145fd6a
Muhammet <134100718+Muhammetsaman@users.noreply.github.com>
2024-02-06 04:49:41 +0300

    Gun 1, Konu: Degiskenler
    Gun 1 , Konu 1

---
+++ Degiskenler.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+
+var ogrenciAdi = "Ahmet"
+var ogrenciYas = 18
+var ogrenciBoy = 1.78
+var ogrenciBasharf = "A"
+var ogrenciDevamEdiyorMu = true
+
+print(ogrenciAdi)
+print(ogrenciYas)
+print(ogrenciBoy)
+print(ogrenciBasharf)
+print(ogrenciDevamEdiyorMu)
+
+
+var urun_id:Int = 3416
+var urun_adi:String = "Macbook Pro"
+var urun_adet:Int = 100
+var urun_fiyat:Int = 34999
+var urun_tedarikci:String = "Apple"
+
+print("Ürün id        : \(urun_id)")
+print("Ürün adı       : \(urun_adi)")
+print("Ürün adet      : \(urun_adet)")
+print("Ürün fiyat     : \(urun_fiyat) ₺")
+print("Ürün tedarikçi : \(urun_tedarikci)")
+
+
+
+
+var sayi = 10
+print(sayi)
+sayi = 20
+print(sayi)
+
+let numara = 100
+print(numara)
+

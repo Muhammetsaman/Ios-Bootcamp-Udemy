1ba74e01eadaf082d557cdac98c8635f7e6b3b17
Muhammet <134100718+Muhammetsaman@users.noreply.github.com>
2024-02-19 12:27:46 +0300

    ios Giris
    gun 4 ders4 (Notlar)

---
+++ iosgiris.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+
+
+/*  Bu derste ;
+ 
+    İos arayüzüne giriş yaptım ve kısaca tanıtıldı.
+ 
+    Projeye artık playground üzerinden devam etmeyeceğim için burayı not olarak kullanacağım
+ 
+    cihaz değişimini öğrendim
+ 
+    simulator ile ilgili yeni bilgiler öğrendim
+ 
+    storyboard üzerinden ilerleyeceğiz ancak kursun sonunda muhtemelen SwiftUİ öğreneceğim
+ 
+    tasarım ile ilgili ufak bilgilerden bahsedildi
+ 
+    Renk seçimleri için bazı web siteleri tavsiye edildi (adobe color vs.)
+ 
+    tema ve şema fikirleri için Uplabs web sitesini sık sık ziyaret edeceğim gibi duruyor.
+    
+    Normal ve dark mod için renk değişimlerini gördüm.
+ 
+    pizza satın alma ekranı için çalışmalara başladık.
+ 
+ */

2217c8d00c86b52c8f69f54b8f06e19679be167b
Muhammet <134100718+Muhammetsaman@users.noreply.github.com>
2024-02-23 05:16:18 +0300

    ios giris 2
    6. gun notları

---
+++ iosgiris2.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+
+
+
+        //          İos Giris 2. gün
+         
+
+
+
+
+
+/*     DERS 1
+ 
+ 
+ ilk gün sayfa oluşturmayı, bunlara text-buton aktarmayı ve sayfalar arası geçiş yapmayı öğrenmiştim.
+ 
+ Öğrendiğim kod yapılarını yeni açtığım github reposunda saklayacağım ve yeri geldiğinde açıp yararlanacağım.
+ 
+ 2. günün ilk dersinde sayfalar arası veri transferi yapmaya başladım.
+ 
+    -Anasayfada, özelliklerini oluşturduğum içi boş classıma değer atayarak 2. sayfaya aktardım.
+ 
+ Yaşam döngüsü metodunu görmüş oldum.
+ 
+ Navigation controller ile geçiş özelliklerini sağlamıştım dün. Bugün ise derste TabBar konusuna da giriş yaptım.
+        -TabBar çok sık kullanılan bir özellik. Hemen hemen bütün uygulamalarda var diyebiliriz.
+ 
+ 2. derste tabBar konusunun detayına ineceğim ve Navigation Controller ile beraber kullanımını göreceğim.
+ 
+ 
+ 
+ */
